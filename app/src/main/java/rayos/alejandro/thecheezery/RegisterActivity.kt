274d90b575964e807a88c66a3987b5d7a342eeb1
rsarayos <raul.sauceda@potros.itson.edu.mx>
2025-02-05 11:44:01 -0700

    Actividad

---
+++ app/src/main/java/rayos/alejandro/thecheezery/RegisterActivity.kt
@@ -0,0 +1,29 @@
+package rayos.alejandro.thecheezery
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val button: Button = findViewById(R.id.sign_button) as Button
+
+        button.setOnClickListener{
+            val intent = Intent(this@RegisterActivity, MenuActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
