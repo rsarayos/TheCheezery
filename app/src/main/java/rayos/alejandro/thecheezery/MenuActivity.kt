7bce82e7568f867994a354494c939a771bdc4b67
rsarayos <raul.sauceda@potros.itson.edu.mx>
2025-02-11 19:30:41 -0700

    Avance Video
    Se termina hasta lo mostrado en el video

--- app/src/main/java/rayos/alejandro/thecheezery/MenuActivity.kt
+++ app/src/main/java/rayos/alejandro/thecheezery/MenuActivity.kt
@@ -1,6 +1,8 @@
 package rayos.alejandro.thecheezery
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,14 @@ class MenuActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_menu)
+
+        var btnCold: Button = findViewById(R.id.button_cold_drinks) as Button
+
+        btnCold.setOnClickListener {
+            var intent: Intent = Intent(this, ProductosActivity::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
---
+++ app/src/main/java/rayos/alejandro/thecheezery/Product.kt
@@ -0,0 +1,6 @@
+package rayos.alejandro.thecheezery
+
+data class Product (var name: String,
+                    var image: Int,
+                    var description: String,
+                    var price: Double)
